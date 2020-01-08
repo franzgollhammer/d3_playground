967b1b8f8ba814d4894d580b076a2530a3f5b359
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-01-08 10:02:54 +0100

    small style tweaks

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 d3 = require("d3")
 _ = require("lodash")
 
-const width = 800
-const height = 400
+const width = 1000
+const height = 600
 
 // Get svg element
 const svg = d3.select("svg")
@@ -16,9 +16,9 @@ const render = (data) => {
     const yValue = d => d.name
 
     const margin = {
-        top: 20,
+        top: 50,
         right: 20,
-        bottom: 20,
+        bottom: 50,
         left: 100
     }
 
@@ -40,16 +40,30 @@ const render = (data) => {
         .padding(0.1)
 
     // Axis
-    const xAxis = d3.axisBottom(xScale)
+    const xAxisTickFormat = number => d3.format(".3s")(number).replace("G", "B") // format ticks
+    const xAxis = d3.axisBottom(xScale).tickFormat(xAxisTickFormat)
+        .tickSize(-innerHeight)
     const yAxis = d3.axisLeft(yScale)
 
     const g = svg.append("g")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
-    
 
-        g.append("g").call(yAxis)
-        g.append("g").call(xAxis)
-            .attr("transform", `translate(0, ${innerHeight})`)
+    g.append("g").call(yAxis)
+        .selectAll(".domain, .tick line") // remove unnecessary lines
+        .remove()
+
+    const xAxisG = g.append("g").call(xAxis)
+        .attr("transform", `translate(0, ${innerHeight})`)
+        
+    xAxisG.selectAll(".domain")
+        .remove()
+
+    xAxisG.append("text")
+        .attr("y", 40)
+        .attr("x", innerWidth / 2)
+        .text("Population")
+        .attr("fill", "black")
+        .attr("font-size", "20px")
 
     g.selectAll("rect").data(data)
         .enter()
@@ -57,16 +71,23 @@ const render = (data) => {
         .attr("width", d => xScale(xValue(d)))
         .attr("height", yScale.bandwidth())
         .attr("y", d => yScale(yValue(d)))
-        .attr("fill",d => colorScale(xValue(d)))
+        .attr("fill", d => colorScale(xValue(d)))
 
-    
+    g.append("text")
+        .text("Estimated population")
+        .attr("font-size", "30px")
+        .attr("text-align", "center")
+        .attr("x", innerWidth / 2 - margin.left)
+        .attr("y", -10)
+        .attr("font-family", "arial")
+
 }
 
 // Fetch data from API and clean it up
 const getData = async () => {
     data = await d3.tsv("https://cdn.jsdelivr.net/npm/world-atlas@1/world/110m.tsv")
-    let countries = data.map(c => ({name: c.name, pop: +c.pop_est}))
-        .sort((a,b ) => b.pop - a.pop)
+    let countries = data.map(c => ({ name: c.name, pop: +c.pop_est }))
+        .sort((a, b) => b.pop - a.pop)
         .slice(0, 10)
     console.log(countries)
     render(countries)
